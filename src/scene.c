60d062d4fb35f33a89012a9b6d0c804daf51dcc7
Christian Döring <doering.christian.2001@gmx.de>
2021-12-07 11:45:17 +0100

    Added scene.c

---
+++ src/scene.c
@@ -0,0 +1,31 @@
+#include "scene.h"
+
+int scene_init(struct scene *dst){
+    darray_init(&dst->meshes, 10);
+
+    dst->shader = NULL;
+
+    texture_init(&dst->lights, 0, 2, NULL);
+
+    return 0;
+}
+void scene_free(struct scene *dst){
+    darray_free(&dst->meshes);
+    texture_free(&dst->lights);
+}
+
+int scene_mesh_push_back(struct scene *dst, struct mesh *src){
+    darray_push_back(&dst->meshes, src);
+    src->lights = &dst->lights;
+    return 0;
+}
+
+int scene_draw(struct scene *dst){
+    if(dst->shader == NULL)
+        return 1;
+    glUseProgram(dst->shader->program);
+    for(size_t i = 0;i < darray_len(&dst->meshes);i++){
+        mesh_draw(dst->meshes[i]);
+    }
+    return 0;
+}
